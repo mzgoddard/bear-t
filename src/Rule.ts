8fbc67155c018b297faf1df87db9a5909a720e05
Michael "Z" Goddard <mzgoddard@gmail.com>
2020-01-14 10:23:52 -0500

    wip

---
+++ src/Rule.ts
@@ -0,0 +1,170 @@
+// Arg, Value, Atom, Scope, Frame, Thread
+
+type SyncReturn = boolean | void;
+type AsyncReturn = Promise<SyncReturn>;
+
+class Arg<Name extends string = any, Type = any> {
+    name: Name;
+    make: new () => Type;
+    ruleIndex: number = -1;
+
+    constructor(name: Name, make: new () => Type = null) {
+        this.name = name;
+        this.make = make;
+    }
+
+    static type<Type>(make: new () => Type) {
+        return <Name extends string>(name: Name) => new Arg(name, make);
+    }
+
+    static any<Name extends string>(name: Name) {
+        return new Arg(name);
+    }
+
+    static boolean<Name extends string>(name: Name) {
+        return new Arg(name, Boolean);
+    }
+}
+
+function arg<Name extends string, Type>(name: Name, make?: new () => Type): Arg<Name, Type>;
+function arg<Name extends Arg>(name: Name): Name;
+function arg<Name extends string | Arg, Type>(name: Name, make?: new () => Type): Name extends string ? Arg<Name, Type> : Name extends Arg<infer N, infer T> ? Arg<N, T> : Arg;
+function arg<Name extends string | Arg, Type>(name: Name, make: new () => Type = null) {
+    return typeof name === 'string' ? new Arg(name, make) : name;
+}
+
+type SimpleType<Arg> = Arg extends Boolean ? boolean : Arg extends String ? string : Arg extends Number ? number : Arg;
+type TypeArg<A> = {value: A extends Arg<any, infer Type> ? SimpleType<Type> : any};
+type RuleFuncArgs<Args extends any[]> = {[key in keyof Args]: TypeArg<Args[key]>} & Args[keyof Args][];
+
+type RuleFunc<Args extends TupleOf<Arg> = Arg[], Return extends SyncReturn | AsyncReturn = any> = (thread, ...args: RuleFuncArgs<Args>) => Return;
+
+type TupleOf<T> = ([] | [T] | [T, T] | [T, T, T] | [T, T, T, T] | [T, T, T, T, T] | T[]);
+
+type ArgNamed<A extends string | Arg> = A extends string ? Arg<A, any> : A extends Arg<infer Name, infer Type> ? Arg<Name, Type> : Arg;
+type ArgNamedFrom<CArgs extends TupleOf<string | Arg>> = {[key in Extract<keyof CArgs, number>]: ArgNamed<CArgs[key]>};
+type ArgTupleFrom<CArgs extends TupleOf<string | Arg>> = (
+    CArgs extends [] ? [] :
+    CArgs extends [string | Arg] ? [ArgNamed<CArgs[0]>] :
+    CArgs extends [string | Arg, string | Arg] ? [ArgNamed<CArgs[0]>, ArgNamed<CArgs[1]>] :
+    CArgs extends [string | Arg, string | Arg, string | Arg] ? [ArgNamed<CArgs[0]>, ArgNamed<CArgs[1]>, ArgNamed<CArgs[2]>] :
+    CArgs extends [string | Arg, string | Arg, string | Arg, string | Arg] ? [ArgNamed<CArgs[0]>, ArgNamed<CArgs[1]>, ArgNamed<CArgs[2]>, ArgNamed<CArgs[3]>] :
+    CArgs extends [string | Arg, string | Arg, string | Arg, string | Arg, string | Arg] ? [ArgNamed<CArgs[0]>, ArgNamed<CArgs[1]>, ArgNamed<CArgs[2]>, ArgNamed<CArgs[3]>, ArgNamed<CArgs[4]>] :
+    ArgNamed<CArgs[Extract<keyof CArgs, number>]>[]
+);
+
+class Rule<CArgs extends TupleOf<string | Arg> = (string | Arg)[], Args extends ArgTupleFrom<CArgs> = ArgTupleFrom<CArgs>, Body extends Atom | RuleFunc<Args, any> = RuleFunc<Args, any>, Return extends SyncReturn | AsyncReturn = Body extends RuleFunc<any, infer R> ? R : any> {
+    args: Args;
+    locals: Arg[];
+    body: Body;
+
+    constructor(args: CArgs, body: Body) {
+        this.args = (args as (string | Arg)[]).map(carg => {
+            if (typeof carg === 'string') {
+                return arg(carg);
+            }
+            return carg;
+        }) as Args;
+        this.body = body;
+    }
+}
+
+type RuleBody<Args extends TupleOf<Arg>, Return extends SyncReturn | AsyncReturn> = Atom | RuleFunc<Args, Return>;
+
+const rule = <CArgs extends TupleOf<string | Arg>, Args extends ArgTupleFrom<CArgs>, Body extends RuleBody<Args, any>, Return extends SyncReturn | AsyncReturn = Body extends RuleFunc<any, infer R> ? R : any>(args: CArgs, body: Body): Rule<CArgs, Args, Body, Return> => new Rule(args, body);
+
+const a = Arg.any;
+const b = Arg.type(Boolean);
+const n = Arg.type(Number);
+
+new Rule(['a'], (t, a) => {}).args;
+new Rule([arg('a')], (t, a) => {}).args;
+new Rule([arg('a', Boolean), arg('b', Number)], (t, a, b) => {b.value = Number(a.value)}).args;
+new Rule([b('a'), n('b')], (t, a, b) => {b.value = Number(a.value)}).args;
+rule([arg('a', Boolean), arg('b', Number)], (t, a, b) => {}).args;
+
+// const l = <V extends string | Arg, A extends V[]>(...v: A): A => v;
+// const a = l('a', 'b');
+// const b = l(arg('a'), arg('b', Boolean));
+
+const ll = <V extends string | Arg, A extends V[]>(...v: A) => v;
+rule(ll(arg('a', Boolean), arg('b', Number)), (t, a, b) => {}).args;
+const lll = <V extends Arg, A extends [] | [V] | [V, V] | [V, V, V] | [V, V, V, V] | [V, V, V, V, V] | V[]>(v: A) => v;
+rule(lll([arg('a', Boolean), arg('b', Number)]), (t, a, b) => {}).args;
+
+type AtomMembers<R extends Rule> = R extends Rule<any, infer Args, any, any> ? (Arg | Atom)[] & {length: Args['length']} : (Arg | Atom)[];
+type CAtomMembers<R extends Rule> = R extends Rule<any, infer Args, any, any> ? (string | Arg | Atom)[] & {length: Args['length']} : (string | Arg | Atom)[];
+
+class Atom<R extends Rule = Rule, M extends AtomMembers<R> = any> {
+    rule: R;
+    members: M;
+
+    constructor(rule: R, members: CAtomMembers<R>) {
+        this.rule = rule;
+        this.members = members as M;
+    }
+}
+
+const atom = <R extends Rule, M extends AtomMembers<R>>(rule: R, ...members: CAtomMembers<R>) => new Atom<R, M>(rule, members);
+
+atom(rule([], () => {}));
+atom(rule(['a'], () => {}), arg('a'));
+rule([], atom(rule(['a'], () => {}), arg('a')));
+rule([], atom(rule(['a'], () => {}), 'a'));
+
+class Scope {
+    caller: Atom;
+    values: any[];
+    parent: Scope;
+
+    constructor(atom: Atom);
+    constructor(atom: Atom, parent: Scope);
+    constructor(rule: Rule, values: any[], parent: Scope);
+    constructor() {
+
+    }
+
+    private findIndex(f: (v: Arg) => boolean) {
+        for (let i = 0; i < this.caller.rule.locals.length; i++) {
+            if (f(this.caller.rule.locals[i])) return i;
+        }
+        return -1;
+    }
+
+    get(key: string) {
+        return this.getAt(this.findIndex(a => a.name === key));
+    }
+
+    getAt(index: number) {
+        return this.values[index];
+    }
+
+    set(key: string, value: any) {
+        this.setAt(this.findIndex(a => a.name === key), value);
+    }
+
+    setAt(index: number, value: any) {
+        this.values[index] = value;
+    }
+
+    call(thread, atom: Atom<Rule<any, any, RuleFunc>>) {
+        if (atom.members.length === 0) {
+            return atom.rule.body(thread);
+        } else if (atom.members.length === 1) {
+            const a = atom.members[0] instanceof Arg ? {value: this.values[atom.members[0].ruleIndex]} : atom.members[0];
+            return atom.rule.body(thread, a);
+        }
+        return atom.rule.body(thread, ...atom.members.map(member => member instanceof Arg ? {value: this.values[member.ruleIndex]} : member));
+    }
+
+    clone() {
+        return new Scope(this.rule, this.values.slice(), this.parent);
+    }
+
+    pop() {
+        const parent = this.parent.clone();
+        const caller = this.caller;
+        for (let i = 0; i < )
+        parent.set
+    }
+}
--- src/rule-design.ts
+++ src/rule-design.ts
@@ -1,11 +1,164 @@
-const boolAnd = jsRule([boolArg('left'), boolArg('right')], (a, b) => a.isValue() && b.isValue() && a.value && b.value)
-const boolAnd2 = jsRule({left: Boolean, right: Boolean}, ({left, right}) => left && right)
-const set = jsRule([anyArg('left'), anyArg('right')], (left, right) => left.isNull() && left.write(right) || left.isValue() && left.value === right.value)
-const eq = jsRule([anyArg('left'), anyArg('right')], (left, right) => )
-atom(set, anyArg('a'), 10);
-run(
-    set(anyArg('a'), 10)
-    .then(set(anyArg('b'), add(anyArg('a'), 20))
-    .then(is(anyArg('c'), anyArg('b'))
-)
-rule([anyArg('a'), anyArg('b')], atom(set, anyArg('b'), anyArg('a')))
+// // const boolAnd = jsRule([boolArg('left'), boolArg('right')], (a, b) => a.isValue() && b.isValue() && a.value && b.value)
+// // const boolAnd2 = jsRule({left: Boolean, right: Boolean}, ({left, right}) => left && right)
+// // const set = jsRule([anyArg('left'), anyArg('right')], (left, right) => left.isNull() && left.write(right) || left.isValue() && left.value === right.value)
+// // const eq = jsRule([anyArg('left'), anyArg('right')], (left, right) => )
+// // atom(set, anyArg('a'), 10);
+// // run(
+// //     set(anyArg('a'), 10)
+// //     .then(set(anyArg('b'), add(anyArg('a'), 20))
+// //     .then(is(anyArg('c'), anyArg('b'))
+// // )
+// // rule([anyArg('a'), anyArg('b')], atom(set, anyArg('b'), anyArg('a')))
+
+// class Arg<Type = any> {
+//     name: string;
+// }
+
+// type Value = boolean | string | number | symbol | Arg | Value[] | {[key: string]: Value};
+
+// type Rule = {
+//     (...args: any[]): Atom;
+//     argNames: any[];
+//     body: Atom | Function;
+// };
+
+// type Atom<Tag extends string | Rule = string | Rule, Args extends Value[] = any[]> = {
+//     tag: Tag,
+//     args: Args,
+
+//     and(...args): Atom;
+//     or(...args): Atom;
+// };
+
+// const atom = <Tag extends string | Rule, Args extends Value[]>(tag: Tag, ...args: Args): Atom => ({tag, args});
+
+// const rule = (argNames: any[], body: Atom | Function): Rule => {
+//     const f = Object.assign((...args: any[]) => atom(f, ...args), {
+//         argNames,
+//         body,
+//     });
+//     return f;
+// };
+
+// const arg = (name: string) => ({});
+
+// const value = (v) => ({});
+
+// class ArgSet {
+//     puts: Arg[];
+//     locals: Arg[];
+
+//     get(key: string | Arg) {
+//         const name = key instanceof Arg ? key.name : key;
+//         for (let i = 0; i < this.puts.length; i++) {
+//             if (this.puts[i].name === name) return this.puts[i];
+//         }
+//         for (let i = 0; i < this.locals.length; i++) {
+//             if (this.puts[i].name === name) return this.puts[i];
+//         }
+//         const newArg = new Arg();
+//         this.locals.push(newArg);
+//         return newArg;
+//     }
+
+//     scope(keys: string[]) {
+//         const scope = {};
+//         for (let i = 0; i < keys.length; i++) {
+//             scope[keys[i]] = this.get(keys[i]);
+//         }
+//         return scope;
+//     }
+// }
+
+// const and = (...args) => atom('and', ...args);
+// const or = (...args) => atom('and', ...args);
+// const bound = (...args) => atom('and', ...args);
+
+// rule(
+//     ['a', 'b', 'c'],
+//     and(bound('a'), and(bound('b'),
+//         or(
+//             and(bound('c'), atom(rule(['a', 'b', 'c'], (t, a, b, c) => (c.value === a.value + b.value))),
+//             atom(rule(['a', 'b', 'c'], (t, a, b, c) => {c.value = a.value + b.value;}))
+//         )
+//     )))
+// )
+// // (((a; , b; ), c; ), c is a + b)
+// // (
+// //    (a, ((b, ((c, c = a + b); c is a + b)); b is c - a));
+// //    (
+// //        (
+// //            b,
+// //            (
+// //                (c, a is c - b);
+// //                false
+// //            )
+// //        );
+// //        false
+// //    )
+// // )
+// bound('a')
+//     .and(bound('b')
+//         .and(bound('c')
+//             .and(atom(rule(['a', 'b', 'c'], (t, a, b, c) => (c.value === a.value + b.value)), 'a', 'b', 'c'))
+//             .or(atom(rule([], () => {})))
+//         )
+//         .or(atom(rule([], () => {})))
+//     )
+//     .or(bound('b')
+//         .and()
+//     )
+
+// bound('a')
+//     .and()
+
+// const add = rule([arg('a'), arg('b'), arg('c')], (thread, a, b, c) => {
+//     if (a.isBound()) {
+//         if (b.isBound()) {
+//             if (c.isBound()) {
+//                 return c.value === a.value + b.value;
+//             } else {
+//                 c.value = a.value + b.value;
+//             }
+//         } else {
+//             if (c.isBound()) {
+//                 b.value = c.value - a.value;
+//             } else {
+//                 return false;
+//             }
+//         }
+//     } else {
+//         if (b.isBound()) {
+//             if (c.isBound()) {
+//                 a.value = c.value - b.value;
+//             } else {
+//                 return false;
+//             }
+//         } else {
+//             return false;
+//         }
+//     }
+// })
+// const neg = rule([arg('i'), arg('o')], (thread, i, o) => {
+//     if (i.isBound()) {
+//         if (o.isBound()) {
+//             return o.value === -i.value;
+//         } else {
+//             o.value = -i.value;
+//         }
+//     } else {
+//         if (o.isBound()) {
+//             i.value = -o.value;
+//         } else {
+//             return false;
+//         }
+//     }
+// })
+// const sub = rule([arg('a'), arg('b'), arg('c')], and(neg(arg('b'), arg('nb')), add(arg('a'), arg('nb'), arg('c'))))
+// const add2 = rule([arg('a'), arg('c')],
+//     and(
+//         add(value(2), value(2), arg('b')),
+//         add(arg('a'), arg('b'), arg('c'))
+//     ))
+
+// and({left: add(2, 2, 'b'), right: add('a', 'b', 'c')})
