13bbb108f327bf5218ac3e4156b6392fbc6daa96
Michael "Z" Goddard <mzgoddard@gmail.com>
2020-01-15 18:26:55 -0500

    wip

--- src/Rule.ts
+++ src/Rule.ts
@@ -53,35 +53,44 @@ type ArgTupleFrom<CArgs extends TupleOf<string | Arg>> = (
     ArgNamed<CArgs[Extract<keyof CArgs, number>]>[]
 );
 
-class Rule<CArgs extends TupleOf<string | Arg> = (string | Arg)[], Args extends ArgTupleFrom<CArgs> = ArgTupleFrom<CArgs>, Body extends Atom | RuleFunc<Args, any> = RuleFunc<Args, any>, Return extends SyncReturn | AsyncReturn = Body extends RuleFunc<any, infer R> ? R : any> {
+type Rule<Args extends TupleOf<Arg> = any, Body extends Atom | RuleFunc<Args, any> = any, Return extends SyncReturn | AsyncReturn = Body extends RuleFunc<any, infer R> ? R : any> = {
+    (...args: CAtomMembers<Rule<Args, Body>>): Atom<Rule<Args, Body>>;
+    // (): 
+    // (...args): Promise<Values<{[key in Extract<keyof Args, number>]: Args[key] extends Arg<infer Name, infer Type> ? {[key in Name]: Type} : any}>>;
     args: Args;
     locals: Arg[];
     body: Body;
+};
 
-    constructor(args: CArgs, body: Body) {
-        this.args = (args as (string | Arg)[]).map(carg => {
+type RuleBody<Args extends TupleOf<Arg>, Return extends SyncReturn | AsyncReturn> = Atom | RuleFunc<Args, Return>;
+
+const rule = <CArgs extends TupleOf<string | Arg>, Args extends ArgTupleFrom<CArgs>, Body extends RuleBody<Args, any>, Return extends SyncReturn | AsyncReturn = Body extends RuleFunc<any, infer R> ? R : any>(args: CArgs, body: Body): Rule<Args, Body, Return> => {
+    const r: Rule<Args, Body, Return> = Object.assign((...args: CAtomMembers<Rule<Args, Body, Return>>) => atom(r, ...args), {
+        args: (args as (string | Arg)[]).map(carg => {
             if (typeof carg === 'string') {
                 return arg(carg);
             }
             return carg;
-        }) as Args;
-        this.body = body;
-    }
-}
-
-type RuleBody<Args extends TupleOf<Arg>, Return extends SyncReturn | AsyncReturn> = Atom | RuleFunc<Args, Return>;
-
-const rule = <CArgs extends TupleOf<string | Arg>, Args extends ArgTupleFrom<CArgs>, Body extends RuleBody<Args, any>, Return extends SyncReturn | AsyncReturn = Body extends RuleFunc<any, infer R> ? R : any>(args: CArgs, body: Body): Rule<CArgs, Args, Body, Return> => new Rule(args, body);
+        }) as Args,
+        locals: [],
+        body,
+    });
+    return r;
+};
 
 const a = Arg.any;
 const b = Arg.type(Boolean);
 const n = Arg.type(Number);
 
-new Rule(['a'], (t, a) => {}).args;
-new Rule([arg('a')], (t, a) => {}).args;
-new Rule([arg('a', Boolean), arg('b', Number)], (t, a, b) => {b.value = Number(a.value)}).args;
-new Rule([b('a'), n('b')], (t, a, b) => {b.value = Number(a.value)}).args;
+rule(['a'], (t, a) => {}).args;
+rule([arg('a')], (t, a) => {}).args;
+rule([arg('a', Boolean), arg('b', Number)], (t, a, b) => {b.value = Number(a.value)}).args;
+rule([b('a'), n('b')], (t, a, b) => {b.value = Number(a.value)}).args;
 rule([arg('a', Boolean), arg('b', Number)], (t, a, b) => {}).args;
+const am = rule([arg('a', Boolean), arg('b', Number)], (t, a, b) => {});
+const am1 = am('a', 'b');
+
+type tm1 = Parameters<typeof am>;
 
 // const l = <V extends string | Arg, A extends V[]>(...v: A): A => v;
 // const a = l('a', 'b');
@@ -92,25 +101,27 @@ rule(ll(arg('a', Boolean), arg('b', Number)), (t, a, b) => {}).args;
 const lll = <V extends Arg, A extends [] | [V] | [V, V] | [V, V, V] | [V, V, V, V] | [V, V, V, V, V] | V[]>(v: A) => v;
 rule(lll([arg('a', Boolean), arg('b', Number)]), (t, a, b) => {}).args;
 
-type AtomMembers<R extends Rule> = R extends Rule<any, infer Args, any, any> ? (Arg | Atom)[] & {length: Args['length']} : (Arg | Atom)[];
-type CAtomMembers<R extends Rule> = R extends Rule<any, infer Args, any, any> ? (string | Arg | Atom)[] & {length: Args['length']} : (string | Arg | Atom)[];
+type TupleN<T, N extends number> = (N extends 0 ? [] : N extends 1 ? [T] : N extends 2 ? [T, T] : N extends 3 ? [T, T, T] : N extends 4 ? [T, T, T, T] : T[]);
 
-class Atom<R extends Rule = Rule, M extends AtomMembers<R> = any> {
+type AtomMembers<R extends Rule> = TupleN<(Arg | Atom), R extends Rule<infer Args, any, any> ? Args['length'] : number>;
+type CAtomMembers<R extends Rule> = TupleN<(string | Rule<[]> | Arg | Atom), R extends Rule<infer Args, any, any> ? Args['length'] : number>;
+
+type Values<V> = V[keyof V];
+
+type Atom<R extends Rule = Rule, M extends AtomMembers<R> = any> = {
     rule: R;
     members: M;
+};
 
-    constructor(rule: R, members: CAtomMembers<R>) {
-        this.rule = rule;
-        this.members = members as M;
-    }
-}
-
-const atom = <R extends Rule, M extends AtomMembers<R>>(rule: R, ...members: CAtomMembers<R>) => new Atom<R, M>(rule, members);
+const atom = <R extends Rule<any, any, any>>(rule: R, ...members: CAtomMembers<R>) => {
+    return Object.assign(() => ({}), {rule, members} as Atom<R, AtomMembers<R>>);
+};
 
 atom(rule([], () => {}));
 atom(rule(['a'], () => {}), arg('a'));
 rule([], atom(rule(['a'], () => {}), arg('a')));
 rule([], atom(rule(['a'], () => {}), 'a'));
+atom(rule(['a'], () => {}), 'b')
 
 class Scope {
     caller: Atom;
@@ -131,7 +142,11 @@ class Scope {
         return -1;
     }
 
-    get(key: string) {
+    get(key: string): any;
+    get<T>(key: Arg<any, T>): SimpleType<T>;
+    get(key: Arg): any;
+    get(key: string | Arg) {
+        if (key instanceof Arg) return this.get(key.name);
         return this.getAt(this.findIndex(a => a.name === key));
     }
 
@@ -139,7 +154,11 @@ class Scope {
         return this.values[index];
     }
 
-    set(key: string, value: any) {
+    set(key: string, value: any): void;
+    set<T>(key: Arg<any, T>, value: SimpleType<T>): void;
+    set(key: Arg, value: any): void;
+    set(key: string | Arg, value: any) {
+        if (key instanceof Arg) return this.setAt(key.name, value);
         this.setAt(this.findIndex(a => a.name === key), value);
     }
 
@@ -147,15 +166,15 @@ class Scope {
         this.values[index] = value;
     }
 
-    call(thread, atom: Atom<Rule<any, any, RuleFunc>>) {
-        if (atom.members.length === 0) {
-            return atom.rule.body(thread);
-        } else if (atom.members.length === 1) {
-            const a = atom.members[0] instanceof Arg ? {value: this.values[atom.members[0].ruleIndex]} : atom.members[0];
-            return atom.rule.body(thread, a);
-        }
-        return atom.rule.body(thread, ...atom.members.map(member => member instanceof Arg ? {value: this.values[member.ruleIndex]} : member));
-    }
+    // call(thread, atom: Atom<Rule<any, any, RuleFunc>>) {
+    //     if (atom.members.length === 0) {
+    //         return atom.rule.body(thread);
+    //     } else if (atom.members.length === 1) {
+    //         const a = atom.members[0] instanceof Arg ? {value: this.values[atom.members[0].ruleIndex]} : atom.members[0];
+    //         return atom.rule.body(thread, a);
+    //     }
+    //     return atom.rule.body(thread, ...atom.members.map(member => member instanceof Arg ? {value: this.values[member.ruleIndex]} : member));
+    // }
 
     clone() {
         return new Scope(this.caller, this.values.slice(), this.parent);
@@ -163,9 +182,25 @@ class Scope {
 
     pop() {
         const parent = this.parent.clone();
-        const caller = this.caller;
-        for (let i = 0; i < caller.members.length; i++) {
-            parent.set
+        const {members, rule} = this.caller;
+        for (let i = 0; i < members.length; i++) {
+            if (members[i] instanceof Arg) {
+                parent.set(members[i], this.get(rule.args[i]));
+            }
         }
     }
+}
+
+class Frame {
+    atom: Atom;
+    next: Frame;
+
+    constructor(atom: Atom, next: Frame = null) {
+        this.atom = atom;
+        this.next = next;
+    }
+}
+
+class Thread {
+
 }
